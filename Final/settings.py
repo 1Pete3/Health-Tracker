e2581cdebc60ca554d32d6aaa4ee3fc6f2b47b2a
1Pete3 <75858369+1Pete3@users.noreply.github.com>
2021-12-09 10:33:51 -0500

    Update settings.py

--- Final/settings.py
+++ Final/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ['static']
+STATIC_ROOT = BASE_DIR / 'static'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
